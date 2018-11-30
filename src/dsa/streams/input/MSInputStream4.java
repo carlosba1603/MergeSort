fbd39723a9f734159daec623aacec532cc04f819
gabimar <gabimar@WINDOWS-3AU9R32>
2018-11-30 10:01:42 +0100

    Memory Mapping code

--- src/dsa/streams/input/MSInputStream4.java
+++ src/dsa/streams/input/MSInputStream4.java
@@ -1,24 +1,99 @@
 package dsa.streams.input;
 
-import dsa.streams.interfaces.MSInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
-public class MSInputStream4 implements MSInputStream {
-
-	@Override
-	public void open(String path) {
-		// TODO Auto-generated method stub
+public class MSInputStream4  {
+	
+	public long B;
+	public MappedByteBuffer MappedBuffer;
+	public int position=0;
+	public int lenght=0;
+	public FileChannel inChannel;
+	
+	public void setB(long B) {
+		this.B=B;
 	}
 
-	@Override
-	public int read_next() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void open(String path) throws Exception {
+		
+			RandomAccessFile is = new RandomAccessFile(path, "r"); //solo lectura
+			inChannel = is.getChannel();
+			
+			MappedBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, position, B); //Se mapea la región al canal creado
+			
+			lenght=(int) new File(path).length();
+			System.out.println("La Longitud inicial del archivo es: "+lenght);
+			
+			// TODO Auto-generated method stub
+		
+	}
+	
+	public int read_next() throws IOException {
+		
+		// TODO Auto-generated method stub	
+		
+		int IntToReturn=0;
+		
+		//System.out.println("Elementos en el buffer: "+MappedBuffer.remaining());
+		
+		if(MappedBuffer.remaining()>=4) {
+			IntToReturn=MappedBuffer.getInt();
+			//System.out.println("XXXXXXX");
+		
+		} else {
+			MappedBuffer.clear(); //Clear buffer to map a new B portion or take the last part of the file (might be less than B)
+			
+			long newB=0;
+			
+			if(lenght-position>=B) {
+				MappedBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, position, B);
+				IntToReturn=MappedBuffer.getInt();
+				//position=position+4;
+			}
+			
+			else {
+				newB=lenght-position;
+				//System.out.println("Este es Longitud: "+ lenght);
+				//System.out.println("Este es position: "+ position);
+				//System.out.println("Este es NewB: "+ newB);
+				MappedBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, position, newB);
+				IntToReturn=MappedBuffer.getInt();
+				
+			}
+			
+		}
+		
+		position=position+4;
+		
+		return IntToReturn;
 	}
 
-	@Override
+
 	public boolean end_of_stream() {
 		// TODO Auto-generated method stub
+		if (position==lenght) {
+			return true;
+		}
+		
 		return false;
 	}
+	
+	
+/*	public static void main(String args[]) throws Exception {
+		
+		MSInputStream4 test = new MSInputStream4();
+		test.open("C:\Users\gabim\Documents\Master\ULB\Database Systems Architecture\Group_Project\data.csv");
+		while(test.end_of_stream()==false) {
+			System.out.println(test.read_next());
+		}
+		
+	}*/
 
 }
--- src/dsa/streams/output/MSOutputStream4.java
+++ src/dsa/streams/output/MSOutputStream4.java
@@ -1,30 +1,153 @@
 package dsa.streams.output;
 
+import java.io.Closeable;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
+import dsa.streams.input.MSInputStream4;
 import dsa.streams.interfaces.MSOutputStream;
+import dsa.streams.interfaces.StreamUtil;
 
 public class MSOutputStream4 implements MSOutputStream {
 
-	private DataOutputStream dos;
+	public long B;
+	public MappedByteBuffer MappedBuffer;
+	public int position=0;
+	public int numElements=0; //Cantidad de elementos a escribir
+	public FileChannel inChannel;
+	ArrayList<MappedByteBuffer> File = new ArrayList();
+	public int lenght;
 	
+	public void setB(long B) {
+		this.B=B;
+	}
+	
+	public void setNumElements(int numElements) {
+		this.numElements=numElements;
+	}
+	
+	public int getPosition() {
+		return position;
+	}
+	
+	public void setLength() {
+		lenght=(int) (numElements*4);
+	}
+	
+	public ArrayList getFile() {
+		return File;
+	}
+		
 	@Override
-	public void create(String path, boolean append) {
+	public void create(String path) throws IOException {
 		// TODO Auto-generated method stub
+		
+		RandomAccessFile is = new RandomAccessFile(path, "rw"); 
+		inChannel = is.getChannel();
+		
+				
+		MappedBuffer = inChannel.map(FileChannel.MapMode.READ_WRITE, position, lenght); //Se mapea la región al canal creado
+		
+		lenght=(int) new File(path).length();
+		
+		File.add(MappedBuffer);
 		
 	}
 
 	@Override
-	public void write(Integer element) {
+	public void write(Integer element) throws IOException {
 		// TODO Auto-generated method stub
+		
+		if(MappedBuffer.remaining()>=4) {
+			MappedBuffer.putInt(element);
+		
+			
+		} else {
+			MappedBuffer.clear(); //Clear buffer to map a new B portion or take the last part of the file (might be less than B)
+			
+			long newB=0;
+			
+			if(lenght-position>=B) {
+				//position=position+4;
+				MappedBuffer = inChannel.map(FileChannel.MapMode.READ_WRITE, position, B);
+				File.add(MappedBuffer);
+				MappedBuffer.putInt(element);
+			}
+			
+			else {
+				newB=lenght-position;
+				//position=position+4;
+				MappedBuffer = inChannel.map(FileChannel.MapMode.READ_WRITE, position, newB);
+				File.add(MappedBuffer);
+				MappedBuffer.putInt(element);
+			}
+			
+		}
+		
+		position=position+4;
+		
+
 		
 	}
 
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-		this.dos.close();
+		
+			MappedBuffer.clear();
+			inChannel.close();
+		
+		
+		
 	}
+	
+	public boolean end_of_stream() {
+		// TODO Auto-generated method stub
+		if (position==lenght) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	public static void main(String args[]) throws Exception {
+		
+		StreamUtil test = new StreamUtil(); // Se crea un objeto de la calse StreamUtil
+		//test.createRandomFile("C:\\Users\\carlo\\OneDrive\\Desktop\\datos3.txt"); // Se crea el archivo usando la clase StreamUitl
+		
+		System.out.println("**********************************");
+		
+		MSInputStream4 msI = new MSInputStream4(); // se crea un objeto de la clase Input para probar la lectura
+		//msI.open("C:\\Users\\carlo\\OneDrive\\Desktop\\datos3.txt"); //Se abre el archivo y se establece el canal en memoria
+		//msI.setB(100); //Se debe establecer el valor de B para probar la lectura, debe ser menor o igual al tamaño del archivo
+		
+		
+		MSOutputStream4 ms = new MSOutputStream4(); //Se crea un objeto de la clase output
+		 
+		ms.setB(500000); // Se tiene que llamar este método para indicar el tamaño B a pasar a memoria NO PUEDE SER MAYOR QUE EL TAMAÑO DEL ARCHIVO
+		ms.setNumElements(200000000);
+		ms.setLength(); // Se establece el tamaño del archivo
+		ms.create("C:\\Users\\cmarin\\Desktop\\datos2.txt"); //Creamos el archivo para escribir los números aleatorios
+		
+		//Dentro del while se pueden probar las operaciones de lectura y escritura
+		while(ms.end_of_stream()==false) { //El objeto de lectura se llama msI (de la clase input) y el de escritura ms (de la clase output)
+			//System.out.println(msI.read_next()); // Lee los enteros y los imprime
+			Integer randomNum = ThreadLocalRandom.current().nextInt( Integer.MIN_VALUE, Integer.MAX_VALUE ); // genera los números a escribir
+			//System.out.println("El numero aleatorio es: "+randomNum.toString());//imprime los números
+			ms.write(randomNum);//Escribe sobre el archivo
+			
+		}
+		ms.close(); //Cierra los buffers y los archivos
+
+	}
+	
 
 }
